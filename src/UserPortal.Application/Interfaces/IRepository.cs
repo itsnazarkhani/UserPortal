b21b5d517c948661ca63d0d49a80cf4a9ed80166
Mohammad Nazarkhani <m.nazarkhani1380@gmail.com>
2025-08-31 20:16:04 +0330

    Refactor IRepository interface to include pagination parameters and cancellation tokens in method signatures

--- src/UserPortal.Application/Interfaces/IRepository.cs
+++ src/UserPortal.Application/Interfaces/IRepository.cs
@@ -12,18 +12,25 @@ public interface IRepository<TEntity> where TEntity : class, IEntity
     /// <summary>
     /// Gets all the records from the db asynchronously.
     /// </summary>
+    /// <param name="take">Number of records to return.</param>
+    /// <param name="page">Number or the page to take records from.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>Retruns all the records.</returns>
-    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<IEnumerable<TEntity>> GetAllAsync(int take, int page, CancellationToken cancellationToken = default);
     /// <summary>
     /// Gets all the records that matches the specified predicate.
     /// </summary>
     /// <param name="predicate">Predicate to search with.</param>
+    /// <param name="take">Number of records to return.</param>
+    /// <param name="page">Number or the page to take records from.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>Returns all the records that match the ceriteria.</returns>
-    Task<IEnumerable<TEntity>> GetWhereAsync(Func<TEntity, bool> predicate, CancellationToken cancellationToken = default);
+    Task<IEnumerable<TEntity>> GetWhereAsync(Func<TEntity, bool> predicate, int take, int page, CancellationToken cancellationToken = default);
     /// <summary>
     /// Gets entitiy with specified Id or returns null.
     /// </summary>
     /// <param name="id">Unique identifier of the entity to find.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>Returns entity with specified id or null if not found.</returns>
     Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
     /// <summary>
@@ -36,18 +43,21 @@ public interface IRepository<TEntity> where TEntity : class, IEntity
     /// Deletes the specified entity.
     /// </summary>
     /// <param name="entity">Entity which to be deleted.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>True if entity deleted successfully, false if entity not-found.</returns>
     bool Delete(TEntity entity, CancellationToken cancellationToken = default);
     /// <summary>
     /// Checks whether an entity exists or not.
     /// </summary>
     /// <param name="id">Unique identifier of the entity to check it's existence.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>True if entity exists, false otherwise.</returns>
     bool Exists(Guid id, CancellationToken cancellationToken = default);
     /// <summary>
     /// Updates entity with new values provided asynchronously
     /// </summary>
     /// <param name="entity">Entity to be updated with new values.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     /// <remarks>It searches with id of the entity therefore id is required and it does not gets updated.</remarks>
     /// <returns>Returns true if entity found and updated, false otherwise.</returns>
     Task<bool> UpdateAsync(TEntity entity);
