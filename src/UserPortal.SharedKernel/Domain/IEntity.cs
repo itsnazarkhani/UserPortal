aae5f28439e8597a5a84a99d6e01f0ea2d615038
Mohammad Nazarkhani <m.nazarkhani1380@gmail.com>
2025-09-03 09:13:56 +0330

    reafactor: add xml documentation for IEntity interface.

--- src/UserPortal.SharedKernel/Domain/IEntity.cs
+++ src/UserPortal.SharedKernel/Domain/IEntity.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace UserPortal.SharedKernel.Domain;
 
+/// <summary>
+/// Interface reperesenting a entity with a id property.
+/// This interface helps to identify entities and also ensure that all entitites of this kind have id property which might be beneficial in some cases.
+/// </summary>
 public interface IEntity
 {
     Guid Id { get; set; }
