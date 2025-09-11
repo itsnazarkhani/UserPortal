3b489b85e9dcc93fbeb90dd486baa05c5eb83b40
Mohammad Nazarkhani <m.nazarkhani1380@gmail.com>
2025-09-11 11:44:18 +0330

    feat: User entity testing with xUnit with Bogus fake data generation

--- test/UserPortal.Core.Tests/EntitiesTests/ApplicationUserTests.cs
+++ test/UserPortal.Core.Tests/EntitiesTests/ApplicationUserTests.cs
@@ -1,4 +1,5 @@
 using System;
+using Bogus;
 using FluentAssertions;
 using UserPortal.Core.Entities;
 
@@ -7,30 +8,16 @@ namespace UserPortal.Core.Tests.EntitiesTests;
 public class ApplicationUserTests
 {
     [Fact]
-    public void RedundentWhiteSpaces_GetsTrimed_WhenSettingFirstAndLastNames()
+    public void CanGet_FullName_WhenProvidingProperValues()
     {
-        // Arrange & Act
-        var whiteSpaceContainingFirstName = " Ali  ";
-        var whiteSpaceContainingLastName = "  Ahmadi  ";
-        var user = new ApplicationUser()
-        {
-            FirstName = whiteSpaceContainingFirstName,
-            LastName = whiteSpaceContainingLastName
-        };
+        // Arrange
+        var faker = new Faker("fa");
+        string fName, lName;
+        fName = faker.Name.FirstName();
+        lName = faker.Name.LastName();
 
-        // Assert
-        user.FirstName.Should().Be(whiteSpaceContainingFirstName.Trim());
-        user.LastName.Should().Be(whiteSpaceContainingLastName.Trim());
-    }
-
-    [Theory]
-    [InlineData("Ali", "Ahmadi")]
-    [InlineData("Ali", " Ahmadi")]
-    [InlineData("Ali ", " Ahmadi ")]
-    public void CanGet_FullName_WhenProvidingProperValues(string fName, string lName)
-    {
         // Act
-        var user = new ApplicationUser()
+        var user = new User()
         {
             FirstName = fName,
             LastName = lName
@@ -38,83 +25,5 @@ public class ApplicationUserTests
 
         // Assert
         user.FullName.Should().Be(fName.Trim() + " " + lName.Trim());
-    }
-
-    [Theory]
-    [InlineData(null, null)]
-    [InlineData(null, "")]
-    [InlineData("", null)]
-    public void FullName_IsNull_WhenBothFirstAndLastNameIsNullOrEmpty(string? fName, string? lName)
-    {
-        // Act
-        var user = new ApplicationUser()
-        {
-            FirstName = fName,
-            LastName = lName
-        };
-
-        // Assert
-        user.FullName.Should().BeNull();
-    }
-
-    [Fact]
-    public void DisplayName_GetsFullName_WhenFullNameIsNotNull()
-    {
-        // Act
-        var user = new ApplicationUser()
-        {
-            FirstName = "Ali",
-            LastName = "Rezaei"
-        };
-
-        // Assert
-        user.DisplayName.Should().Be(user.FullName);
-    }
-
-    [Fact]
-    public void DisplayName_GetsEmail_WhenFullNameIsNull()
-    {
-        // Act
-        var user = new ApplicationUser()
-        {
-            FirstName = "",
-            LastName = "",
-            Email = "example@email.com"
-        };
-
-        // Assert
-        user.DisplayName.Should().Be("example@email.com");
-    }
-
-    [Fact]
-    public void DisplayName_GetsUserName_WhenFullNameAndEmailAreNull()
-    {
-        // Act
-        var user = new ApplicationUser()
-        {
-            FirstName = "",
-            LastName = "",
-            Email = null!,
-            UserName = "username"
-        };
-
-        // Assert
-        user.DisplayName.Should().Be("username");
-    }
-
-    [Fact]
-    public void DisplayName_GetsUnknownUser_WhenFullNameAndEmailAndUserNamesAreNull()
-    {
-        // Act
-        var user = new ApplicationUser()
-        {
-            FirstName = "",
-            LastName = "",
-            Email = null!,
-            UserName = null!
-        };
-
-        // Assert
-        user.DisplayName.Should().Be("Unknown User");
     }
 }
