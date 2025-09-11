5592f31e1c0719f04e0d7f996504d7031adf2d13
Mohammad Nazarkhani <m.nazarkhani1380@gmail.com>
2025-09-11 18:53:30 +0330

    feat: add test cases for UserMappings and fix UserMappings class to actually check for null or empty values (no need for exception throwing because these are response dtos), moving user related constants to DefaultConstants class

---
+++ src/UserPortal.Core/Constants/DefaultConstants.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UserPortal.Core.Constants;
+
+public static class DefaultConstants
+{
+    public static class User
+    {
+        public const string DefaultUserName = "Anonymous";
+        public const string DefaultFirstName = "Unknown";
+    }
+}
--- src/UserPortal.UseCases/Mapping/UserMappings.cs
+++ src/UserPortal.UseCases/Mapping/UserMappings.cs
@@ -1,4 +1,5 @@
 using System;
+using UserPortal.Core.Constants;
 using UserPortal.Core.Entities;
 using UserPortal.UseCases.DTOs;
 
@@ -9,9 +10,6 @@ namespace UserPortal.UseCases.Mapping;
 /// </summary>
 public static class UserMappings
 {
-    private const string DefaultUsername = "Anonymous";
-    private const string DefaultFirstName = "Unknown";
-
     /// <summary>
     /// Maps ApplicationUser entity to a UserProfileDto dto.
     /// </summary>
@@ -19,12 +17,12 @@ public static class UserMappings
     /// <returns>Returns a UserProfileDto dto containing information which will be shown in profile page of users.</returns>
     /// <remarks>If the user is null, default values will be used for all properties.</remarks>
     public static UserProfileDto ToProfileDto(this User? user) =>
-        new(
-            user?.UserName ?? DefaultUsername,
-            user?.FirstName ?? DefaultFirstName,
-            user?.LastName ?? string.Empty,
-            user?.Email ?? $"{DefaultUsername}@noemail.com",
-            user?.AvatarId ?? Guid.Empty);
+      new(
+          string.IsNullOrWhiteSpace(user?.UserName) ? DefaultConstants.User.DefaultUserName : user!.UserName,
+          string.IsNullOrWhiteSpace(user?.FirstName) ? DefaultConstants.User.DefaultFirstName : user!.FirstName,
+          user?.LastName ?? string.Empty,
+          string.IsNullOrWhiteSpace(user?.Email) ? $"{DefaultConstants.User.DefaultUserName}@noemail.com" : user!.Email,
+          user?.AvatarId ?? Guid.Empty);
 
     /// <summary>
     /// Maps ApplicationUser entity to a UserListDto dto.
@@ -33,5 +31,6 @@ public static class UserMappings
     /// <returns>Returns a UserListDto dto which will be shown in users list page.</returns>
     /// <remarks>If the user is null, default values will be used for all properties.</remarks>
     public static UserListDto ToUserListDto(this User? user) =>
-        new(user?.UserName ?? DefaultUsername, user?.AvatarId ?? Guid.Empty);
+        new(string.IsNullOrEmpty(user?.UserName) ? DefaultConstants.User.DefaultUserName : user!.UserName,
+            user?.AvatarId ?? Guid.Empty);
 }
